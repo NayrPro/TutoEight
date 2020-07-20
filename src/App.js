4b9f52d23c8b0ac255598a14352408863c417f05
1iNi1 <nayir94@hotmail.fr>
2020-07-20 05:40:00 +0200

    App file deletion 1

--- src/App.js
+++ src/App.js
@@ -2,31 +2,6 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-
-  const [pos, setPos] = useState([0,0])
-
-  const logMousePosition = e => {
-      console.log(e.clientX, e.clientY)
-      let arrPos = []
-      arrPos[0] = e.clientX
-      arrPos[1] = e.clientY
-      setPos(arrPos)
-    }
-
-  useEffect(() => {
-    console.log("Event on");
-    window.addEventListener("mousemove", logMousePosition)
-
-    return () => {
-      console.log("Nettoyage");
-
-      window.removeEventListener("mousemove", logMousePosition)
-    }
-    
-
-  },[])
-  
-
   return (
     <div className="App">
       Hello World
