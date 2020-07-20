9cca18582962ef506656285bf3ab67d11951232e
1iNi1 <nayir94@hotmail.fr>
2020-07-20 22:26:15 +0200

    onglets commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './App.css';
-
+import Onglets from './Composants/Onglets'
 function App() {
+  let cursorRef = useRef()
+
+  const mousePos = e =>{
+    console.log(cursorRef.current);
+    cursorRef.current.setAttribute("style", `top: ${e.pageY - 20}px; left : ${e.pageX - 20}px;`)
+  }
+
   return (
-    <div className="App">
+    <div onMouseMove={mousePos} className="App">
+      <div ref={cursorRef} className="custom-cursor"></div>
+      <Onglets/>
     </div>
   );
 }
